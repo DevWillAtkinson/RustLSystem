227b43f7dfdf2027f775d479d1762c23a8c1451d
DevWillAtkinson <williamlandonatkinson@outlook.com>
2020-10-28 05:28:02 -0400

    Better turtle implementation.

--- src/main.rs
+++ src/main.rs
@@ -12,34 +12,57 @@ pub fn l_system<'a>(axiom: &str, steps: u32, rules: &dyn Fn(char) -> &'a str) ->
     input.to_string()
 }
 
+pub fn instruct_turtle(turtle: &mut Turtle, instructions: String, dist: f64, rot: f64) {
+    let (mut lX, mut lY, mut sX, mut sY) = (0.0, 0.0, 0.0, 0.0);
+    
+    for i in instructions.chars() {
+        match i {
+            'F' => turtle.forward(dist),
+            'f' => {turtle.pen_up(); turtle.forward(dist); turtle.pen_down()},
+            '+' => turtle.left(rot),
+            '-' => turtle.right(rot),
+            '|' => turtle.right(180.0),
+            '{' => turtle.begin_fill(),
+            '}' => turtle.end_fill(),
+            _ => {}
+        }
+
+        let pos = turtle.position();
+        if pos.x > lX {
+            lX = pos.x;
+        } else if pos.x < sX {
+            sX = pos.x;
+        }
+        if pos.y > lY {
+            lY = pos.y;
+        } else if pos.y < sY {
+            sY = pos.y;
+        }
+        turtle.drawing_mut().set_center(((0.0 - lX - sX) / 2.0, (0.0 - sY - lY) / 2.0))
+    }
+}
+
 fn main() {
     let mut turtle = Turtle::new();
-    turtle.drawing_mut().set_size([600, 600]);
-    turtle.left(90.0);
+    turtle.drawing_mut().set_title("L-System Turtle!");
+    turtle.set_speed("instant");
 
     let axiom = "A";
-    let steps = 6;
+    let steps = 5;
     let rules = |x| match x {
         'A' => "-BF+AFA+FB-",
         'B' => "+AF-BFB-FA+",
+        'F' => "F",
+        'f' => "f",
         '+' => "+",
         '-' => "-",
-        'F' => "F",
+        '|' => "|",
+    //  '[' => "",
+    //  ']' => "",
+        '{' => "{",
+        '}' => "}",
         _ => ""
     };
-    
-    let out = l_system(axiom, steps, &rules);
 
-    println!("{}", out);
-
-    let distance = 8.0;
-    let angle = 90.0;
-    for i in out.chars() {
-        match i {
-            'F' => turtle.forward(distance),
-            '+' => turtle.right(angle),
-            '-' => turtle.left(angle),
-            _ => {}
-        }
-    }
+    instruct_turtle(&mut turtle, l_system(axiom, steps, &rules), 4.0, 90.0);
 }
