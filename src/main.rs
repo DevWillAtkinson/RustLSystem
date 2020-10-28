9735f83b5d91ba1cdd2a9a9f279807b6bbfde2dc
DevWillAtkinson <williamlandonatkinson@outlook.com>
2020-10-28 04:26:06 -0400

    Initial Turtle Implementation.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
-// Compute a Lindenmayer system given an axiom, a number of steps and rules
+#![allow(non_snake_case)]
+extern crate turtle;
+use turtle::Turtle;
 
-fn l_system<'a>(axiom: &str, steps: u32, rules: &dyn Fn(char) -> &'a str) -> String {
+pub fn l_system<'a>(axiom: &str, steps: u32, rules: &dyn Fn(char) -> &'a str) -> String {
     let mut input = String::from(axiom);
     for _ in 0..steps {
         let out = input.chars().map(rules).collect();
@@ -10,30 +12,34 @@ fn l_system<'a>(axiom: &str, steps: u32, rules: &dyn Fn(char) -> &'a str) -> Str
     input.to_string()
 }
 
-
-
 fn main() {
-    
+    let mut turtle = Turtle::new();
+    turtle.drawing_mut().set_size([600, 600]);
+    turtle.left(90.0);
+
     let axiom = "A";
-    let steps = 7;
+    let steps = 6;
     let rules = |x| match x {
-        'A' => "AB",
-        'B' => "A",
-        _ => ""
-    };
-
-    let out = l_system(axiom, steps, &rules);
-    println!("{}", out);
-
-    let axiom = "F";
-    let steps = 2;
-    let rules = |x| match x {
-        'F' => "F+F−F−F+F",
+        'A' => "-BF+AFA+FB-",
+        'B' => "+AF-BFB-FA+",
         '+' => "+",
-        '−' => "−",
+        '-' => "-",
+        'F' => "F",
         _ => ""
     };
-
+    
     let out = l_system(axiom, steps, &rules);
+
     println!("{}", out);
+
+    let distance = 8.0;
+    let angle = 90.0;
+    for i in out.chars() {
+        match i {
+            'F' => turtle.forward(distance),
+            '+' => turtle.right(angle),
+            '-' => turtle.left(angle),
+            _ => {}
+        }
+    }
 }
